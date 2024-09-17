cba5feb2508ac705f7ed341c6d4cf2bcc6bb0c2b
Maybellene <maybelleneaung@gmail.com>
2024-09-17 15:34:08 -0400

    done

--- main.py
+++ main.py
@@ -1,2 +1,49 @@
 def calculate_class_averages(classrooms):
-    pass
+    class_averages = {}
+    for subject, students_scores in classrooms.items():
+        # print(subject)
+        # print(students_scores)
+        scores_list = []
+        if not students_scores:
+            class_averages[subject] = 0
+            continue
+        for scores in students_scores.values():
+            scores_list += scores
+        avg = sum(scores_list)/len(scores_list)
+        avg = round(avg,2)
+        class_averages[subject] = avg
+    print(class_averages)        
+    return(class_averages)
+
+
+
+
+
+
+            
+
+
+
+school_scores = {
+        "Math": {},
+        "Science": {
+            "Alice": [80, 85, 88],
+            "Bob": [78, 82, 85]
+        }
+    }
+
+result = calculate_class_averages(school_scores)
+
+
+  # for subject in classrooms:
+    #     subject_scores = classrooms[subject]
+    #     subject_score_list = []
+    #     for student, scores in subject_scores.items():
+    #         if not student:
+    #             avg[subject] = 0
+    #         subject_score_list += scores
+
+    #     average = sum(subject_score_list)/len(subject_score_list)
+    #     average = round(average,2)
+    #     avg[subject] = average
+    # return avg
